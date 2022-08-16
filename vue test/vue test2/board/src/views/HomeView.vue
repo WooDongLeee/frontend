ada54c31f44feb583e266269ca009a7774f8fba1
yaho1231 <hiimjungle@naver.com>
2022-08-16 13:22:22 +0900

    vue test

---
+++ vue test/vue test2/board/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+<script>
+import apiBoard from '@/api/board';
+
+export default {
+  data(){
+    return {
+      title: "hello?",
+    }
+  },
+  mounted(){
+    apiBoard.getArticle(1)
+    .then(function(response){
+      console.log("getArticle",response);
+    })
+    .catch(function(e){
+      console.log(e);
+    });
+
+    apiBoard.getArticles(0)
+    .then(function(response){
+      console.log("getArticles",response);
+    })
+    .catch(function(e){
+      console.log(e);
+    });
+
+    apiBoard.postArticle("id","title","body")
+    .then(function(response){
+      console.log("postArticles",response);
+    })
+    .catch(function(e){
+      console.log(e);
+    });
+  }
+}
+</script>
